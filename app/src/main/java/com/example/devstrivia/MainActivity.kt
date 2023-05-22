cb690b19995348dfd806022019cecd82d9b8d508
Dev Shah <dev.shah@iitg.ac.in>
2023-05-22 12:07:08 +0530

    Add startup screen

--- app/src/main/java/com/example/devstrivia/MainActivity.kt
+++ app/src/main/java/com/example/devstrivia/MainActivity.kt
@@ -1,99 +1,30 @@
 package com.example.devstrivia
 
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import android.widget.Button
-import android.widget.RadioButton
-import android.widget.RadioGroup
-import android.widget.TextView
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
-var finalPosition = 4
-var currentPosition = 1
-var correctAns = (1 .. 4).random()
-var state = 0
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        showQuestion()
-        currentPosition+=1
 
     }
 
-    private fun showQuestion(){
-
-        val questionList = Question_data.getQuestions()
-        val question: Question = questionList[currentPosition-1]
-        val questionView = findViewById<TextView>(R.id.questionView)
-        val answerButtons = findViewById<RadioGroup>(R.id.radioGroup)
-        val answerButton1 = findViewById<RadioButton>(R.id.radioButton1)
-        val answerButton2 = findViewById<RadioButton>(R.id.radioButton2)
-        val answerButton3 = findViewById<RadioButton>(R.id.radioButton3)
-        val answerButton4 = findViewById<RadioButton>(R.id.radioButton4)
-        answerButtons.clearCheck()
-        answerButton1.text = question.correct_answer
-        answerButton2.text = question.incorrect_answers[0]
-        answerButton3.text = question.incorrect_answers[1]
-        answerButton4.text = question.incorrect_answers[2]
-        correctAns = (1 .. 4).random()
-        when(correctAns){
-            1 -> {correctAns = R.id.radioButton1}
-            2 -> {answerButton2.text = question.correct_answer
-                answerButton1.text = question.incorrect_answers[0]
-                correctAns = R.id.radioButton2}
-            3 -> {answerButton3.text = question.correct_answer
-                answerButton1.text = question.incorrect_answers[1]
-                correctAns = R.id.radioButton3}
-            else -> {answerButton4.text = question.correct_answer
-                answerButton1.text = question.incorrect_answers[2]
-                correctAns = R.id.radioButton4}
+    fun nextPage(view: View) {
+        val name = findViewById<EditText>(R.id.name)
+        if (name.text.toString().isEmpty()){
+            Toast.makeText(this, "Please write a name", Toast.LENGTH_SHORT).show()
         }
-        questionView.text = question.question
-        state = 1
-    }
-
-    fun checkAns(view: View) {
-        if (state != 1){
-            return
+        else{
+            val intent = Intent(this, QuestionsActivity::class.java)
+            startActivity(intent)
+            finish()
         }
-        //Log.i("asdf","${view.id}")
-        //val radioOptionsGroup = findViewById<RadioGroup>(R.id.radioGroup)
-        val selectedId: Int = view.id
-        val correct = findViewById<RadioButton>(correctAns)
-        val selected = findViewById<RadioButton>(selectedId)
-        correct.setBackgroundResource(R.drawable.pngitem_correct)
-        if (selectedId != correctAns){
-            selected.setBackgroundResource(R.drawable.pngitem_wrong)
-        }
-        val button = findViewById<Button>(R.id.nextButton)
-        button.visibility = Button.VISIBLE
-        if (currentPosition == finalPosition+1){ button.text = getString(R.string.go_back) }
-        state = 2
-
-
     }
-
-    fun nextQuestion(view: View) {
-
-        val answerButton1 = findViewById<RadioButton>(R.id.radioButton1)
-        val answerButton2 = findViewById<RadioButton>(R.id.radioButton2)
-        val answerButton3 = findViewById<RadioButton>(R.id.radioButton3)
-        val answerButton4 = findViewById<RadioButton>(R.id.radioButton4)
-        answerButton1.setBackgroundResource(R.drawable.pngitem_584233)
-        answerButton2.setBackgroundResource(R.drawable.pngitem_584233)
-        answerButton3.setBackgroundResource(R.drawable.pngitem_584233)
-        answerButton4.setBackgroundResource(R.drawable.pngitem_584233)
-        val button = findViewById<Button>(view.id)
-        if (currentPosition == finalPosition+1){ currentPosition = 1
-            button.text = getString(R.string.next)}
-        showQuestion()
-        currentPosition+=1
-        button.visibility = Button.GONE
-    }
-
-
 
 
 }
